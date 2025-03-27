b1e17e513e8fcf086ae36f5f73b66cdd50dd9d9e
Krishna Ajith <142655890+krishnajithkicha@users.noreply.github.com>
2025-03-27 22:59:11 +0530

    Create Lagrange.py

---
+++ LabCycle2/Lagrange.py
@@ -0,0 +1,26 @@
+#lagrange interpolation
+import math
+def lagrange(x, y, n,p):
+  sol=0.0
+  for i in range(n):
+    num=1.0
+    den=1.0
+    for j in range(n):
+      if i!=j: 
+        num*=(p-x[j])
+        den*=(x[i]-x[j]) # prevent division by zero
+    sol+=(num/den)*y[i]
+  print("The value of f(x) at point ",p," is:", sol)
+
+
+
+  
+n=int(input("Enter the number of values:"))
+x=[]
+for i in range(n):
+  x.append(float(input("Enter the value of x:")))
+y=[]
+for i in range(n):
+  y.append(float(input("Enter the value of y:")))
+p=float(input("Enter the point:"))
+lagrange(x, y, n, p)
